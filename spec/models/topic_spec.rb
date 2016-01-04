082c567438912d45ec1e3e4f200622e49793300b
Wen-Tien Chang <ihower@gmail.com>
2016-01-04 12:49:46 +0800

    Add Topic#tag_list spec

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,5 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Topic, :type => :model do
 
+  describe "#tag_list" do
+
+    it "should return tag list" do
+      topic = Topic.create!( :title => "foo", :content => "qq")
+
+      t1 = Tag.create!( :name => "ruby" )
+      t2 = Tag.create!( :name => "css" )
+      topic.tags = [t1,t2]
+
+      expect( topic.tag_list ).to eq("ruby,css")
+    end
+
+  end
 
 end
